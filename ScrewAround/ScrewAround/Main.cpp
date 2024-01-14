57202c74e5ec4bf5c83a8c0993d372d063e243c3
ericsherman4 <esherman2018@gmail.com>
2024-01-14 09:50:45 -0500

    unique pointer fun

--- ScrewAround/ScrewAround/Main.cpp
+++ ScrewAround/ScrewAround/Main.cpp
@@ -1,40 +1,94 @@
-#include <algorithm>
-#include <vector>
+#include <memory>
 #include <string>
 #include <iostream>
 
-void print(const std::vector<std::string>& vec)
+class Thing
 {
-	std::cout << "start:" << std::endl;
-	std::copy(vec.begin(), vec.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-	std::cout << ":end" << std::endl;
+public:
+    Thing()
+    {
+        std::cout << "ctor called" << std::endl;
+    }
+
+    Thing(const Thing&)
+    {
+        std::cout << "copy ctr called" << std::endl;
+    }
+
+    ~Thing()
+    {
+        std::cout << "dtor called" << std::endl;
+    }
+
+    void mint() const
+    {
+        std::cout << "mint!\n";
+    }
+};
+
+void f(const Thing t)
+{
+    std::cout << "f(): ";
+    t.mint();
 }
 
-int main()
+void g(const Thing& t)
 {
-	// move things to new vector if it is > 10 characters
-	std::vector<std::string> from = { "heydad", "my name is smithy", "jack sparrow is cool", "hey i love you"};
-	std::vector<std::string> dest;
-	dest.reserve(from.size());
+    std::cout << "g(): ";
+    t.mint();
+}
 
-	print(from);
+void h(const Thing* t)
+{
+    std::cout << "h(): ";
+    t->mint();
+}
 
-	std::copy_if(
-		std::make_move_iterator(from.begin()),
-		std::make_move_iterator(from.end()),
-		std::back_inserter(dest), 
-		//dest.end(), // this doesn't work because you have the begin pointer 
-		// pointing to element 0 and the end pointer points to element 1 because 
-		// the vector is empty. So it fails after two iterations.
-		[](const std::string & str)
-		{
-			return str.length() > 10;
-		}
-	);
+class ThingGrabber
+{
+public:
 
-	print(from);
-	print(dest);
+    // recommended to pass unique pointers by value
+    //YOU CANNOT MAKE A COPY OF A UNIQUE POINTER DOESNT MAKE SENSE
+    // need to use move semantics!
+    void Give(std::unique_ptr<Thing> t)
+    {
+        pThing = std::move(t);
+    }
+    std::unique_ptr<Thing> Take()
+    {
+        return std::move(pThing);
+    }
+private:
+    std::unique_ptr<Thing> pThing;
+};
 
-	std::cin.get();
-	return 0;
+// typical OOP factory function
+std::unique_ptr<Thing> MakeThing()
+{
+    // notice dont need to do move semantics if you define a local variable and then return that
+    // compiler realizes it will die anyway so will move resources for you.
+    // if you are returning directly, its a temporary so its an rvalue reference already
+    return std::make_unique<Thing>();
+}
+
+int main() 
+{
+    // Important to note that classes with unique pointers can't be copied because it will be blocked!
+    // no copy constrcutor defined for unique pointer. but move constructor will work.
+
+    {
+        ThingGrabber tg;
+#if 0        
+        auto t = MakeThing();
+        tg.Give(std::move(t));
+#else
+        // gets passed as an rvalue since its a temporary
+        tg.Give(MakeThing());
+#endif
+        tg.Take();
+    }
+
+    std::cin.get();
+    return 0;
 }
